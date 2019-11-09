1d2c0aa4765ca109b91b0b48410a7d7a28772a11
hjc990805 <3091248685@qq.com>
2019-11-09 23:32:35 +0800

    item

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.flexslider').flexslider({
         // 两秒钟自动切换
-        slideshowSpeed: 2500,
+        slideshowSpeed: 3000,
         // 手动切换后继续轮播
         pauseOnAction: false,
         // 显示左右按钮
