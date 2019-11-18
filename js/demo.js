11ef917f4ba458f182a5479637daaf7e4e030ec6
hjc990805 <3091248685@qq.com>
2019-11-18 23:35:53 +0800

    动画完成

--- js/demo.js
+++ js/demo.js
@@ -7,4 +7,38 @@ $(function(){
         // 显示左右按钮
         directionNav: false 
     });
+// 轮播部分
+
+    var num = 0;
+    var mytimer = setInterval(myanimate, 5000);
+
+    function myanimate(){
+        num=num>=3?0:num+1;
+        todo(num);
+    }
+
+    function todo(num){
+        $('.swiper').stop(true,false).animate({left: -num*992 +'px'}, 500);
+    }
+
+    $('.on_left').click(function(){
+        clearInterval(mytimer);
+        num=num<1?0:--num;
+        todo(num);
+    });
+    $('.on_right').click(function(){
+        clearInterval(mytimer);
+        num=num>2?3:++num;
+        todo(num);
+    });
+    
+    $('.on_left,.on_right').mouseout(function(){
+        mytimer = setInterval(myanimate, 5000);;
+    })
+   
+        
+
+
+// 轮播部分 end
+
 })
